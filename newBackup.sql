59103d244691b62bfbf0e697ca362563cdc0d147
sarthak <sg622857@gmail.com>
2024-03-20 21:52:23 +0530

    Database Backup

---
+++ newBackup.sql
@@ -0,0 +1,205 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for macos14 (arm64)
+--
+-- Host: localhost    Database: LMS
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Admin`
+--
+
+DROP TABLE IF EXISTS `Admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Admin` (
+  `AdminID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int DEFAULT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `PhoneNumber` varchar(15) DEFAULT NULL,
+  `Gender` enum('Male','Female','Other') DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`AdminID`),
+  KEY `UserID` (`UserID`),
+  CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Admin`
+--
+
+LOCK TABLES `Admin` WRITE;
+/*!40000 ALTER TABLE `Admin` DISABLE KEYS */;
+INSERT INTO `Admin` VALUES (1,4,'John','Doe','123-456-7890','Male','john.doe@example.com');
+/*!40000 ALTER TABLE `Admin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Book`
+--
+
+DROP TABLE IF EXISTS `Book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Book` (
+  `BookID` int NOT NULL AUTO_INCREMENT,
+  `Title` varchar(100) NOT NULL,
+  `Author` varchar(100) NOT NULL,
+  `AvailableCopies` int NOT NULL,
+  `TotalCopies` int NOT NULL,
+  PRIMARY KEY (`BookID`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Book`
+--
+
+LOCK TABLES `Book` WRITE;
+/*!40000 ALTER TABLE `Book` DISABLE KEYS */;
+INSERT INTO `Book` VALUES (3,'To Kill a Mockingbird','Harper Lee',10,10),(4,'1984','George Orwell',12,12),(5,'Sex ki kahani','Gursahiba Kaur',10,10);
+/*!40000 ALTER TABLE `Book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BorrowedBooks`
+--
+
+DROP TABLE IF EXISTS `BorrowedBooks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `BorrowedBooks` (
+  `BorrowID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int DEFAULT NULL,
+  `BookID` int DEFAULT NULL,
+  `BorrowDate` date NOT NULL,
+  `ReturnDate` date DEFAULT NULL,
+  `Status` enum('Borrowed','Returned') NOT NULL,
+  PRIMARY KEY (`BorrowID`),
+  KEY `UserID` (`UserID`),
+  KEY `BookID` (`BookID`),
+  CONSTRAINT `borrowedbooks_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`),
+  CONSTRAINT `borrowedbooks_ibfk_2` FOREIGN KEY (`BookID`) REFERENCES `Book` (`BookID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BorrowedBooks`
+--
+
+LOCK TABLES `BorrowedBooks` WRITE;
+/*!40000 ALTER TABLE `BorrowedBooks` DISABLE KEYS */;
+INSERT INTO `BorrowedBooks` VALUES (1,6,5,'2024-03-20','2024-03-20','Returned');
+/*!40000 ALTER TABLE `BorrowedBooks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Librarian`
+--
+
+DROP TABLE IF EXISTS `Librarian`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Librarian` (
+  `LibrarianID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int DEFAULT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `PhoneNumber` varchar(15) DEFAULT NULL,
+  `Gender` enum('Male','Female','Other') DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`LibrarianID`),
+  KEY `UserID` (`UserID`),
+  CONSTRAINT `librarian_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Librarian`
+--
+
+LOCK TABLES `Librarian` WRITE;
+/*!40000 ALTER TABLE `Librarian` DISABLE KEYS */;
+INSERT INTO `Librarian` VALUES (1,5,'Jane','Smith','987-654-3210','Female','jane.smith@example.com');
+/*!40000 ALTER TABLE `Librarian` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Student`
+--
+
+DROP TABLE IF EXISTS `Student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Student` (
+  `StudentID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int DEFAULT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `PhoneNumber` varchar(15) DEFAULT NULL,
+  `Gender` enum('Male','Female','Other') DEFAULT NULL,
+  `Email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`StudentID`),
+  KEY `UserID` (`UserID`),
+  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `User` (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Student`
+--
+
+LOCK TABLES `Student` WRITE;
+/*!40000 ALTER TABLE `Student` DISABLE KEYS */;
+INSERT INTO `Student` VALUES (1,6,'Alice','Johnson','555-123-4567','Female','alice.johnson@example.com'),(2,7,'Bob','Williams','555-987-6543','Male','bob.williams@example.com');
+/*!40000 ALTER TABLE `Student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `User` (
+  `UserID` int NOT NULL AUTO_INCREMENT,
+  `Username` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `UserType` enum('Admin','Librarian','Student') NOT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (4,'admin','admin123','Admin'),(5,'librarian','librarian123','Librarian'),(6,'student1','student123','Student'),(7,'student2','student456','Student');
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-20 21:51:46
